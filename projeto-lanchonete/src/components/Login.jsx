fb619f1f4314590fe76ed2bc7c6d4fd97110217b
unknown <peruzzo.code@gmail.com>
2026-06-23 17:15:01 -0300

    terceiro commit

--- projeto-lanchonete/src/components/Login.jsx
+++ projeto-lanchonete/src/components/Login.jsx
@@ -1,8 +1,12 @@
 function Login(){
     return(
         <>
+        <div className="container">
             <h2>Login</h2>
-            
+            <input type="text" placeholder="Usuário" />
+            <input type="text" placeholder="Senha" />
+            <button>Enviar</button>
+        </div>   
             
         </>
         
