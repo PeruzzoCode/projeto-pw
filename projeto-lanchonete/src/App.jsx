b95d766a37d0ea920cafd7b6ca84d1a8fa7062d8
unknown <peruzzo.code@gmail.com>
2026-06-23 16:16:32 -0300

    first commit

---
+++ projeto-lanchonete/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App(){
+    return(
+        <>
+            <Header titulo="Lanchonete e Dog" 
+            subtitulo="O melhor da região"/>
+            
+            <Login />
+
+
+
+        </>
+    )
+}
+export default App
---
+++ projeto-lanchonete/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header({titulo, subtitulo}){
+    return(
+        <>
+            <h1>{titulo}</h1>
+            <h2>{subtitulo}</h2>
+        </>
+    )
+}
+export default Header
