6fc5ecf86aa11e288f55960f2d57190b14bcaee4
PeruzzoCode <peruzzo.code@gmail.com>
2026-07-29 17:08:42 -0300

    adicionando imagem

--- projeto-lanchonete/src/App.jsx
+++ projeto-lanchonete/src/App.jsx
@@ -4,12 +4,21 @@ import CardProd from "./components/CardProd";
 import Funcionario from "./components/Funcionario";
 import Contador from "./components/Contador";
 import Calculadora from "./components/Calculadora";
+import './App.css'
 
 function App(){
 
-    const lanches = [{id:1, nome: "X-salada", preco: "17.50"},
-        {id:2,nome:"X-burguer", preco: "15.90"},
-        {id:3, nome: "Refrigerante", preco: "6.00"}]
+    const lanches = [
+        {id:1, nome: "X-salada", preco: "17.50", imagem: "/images/x-salada.jpg" },
+        {id:2,nome:"X-burguer", preco: "15.90", imagem: "/images/x-burguer.jpg"},
+        {id:3, nome: "Refrigerante", preco: "6.00", imagem: "/images/refrigerante.png"}
+    ]
+
+    const funcionario = [
+        {id:1, nome: "Felipe", cargo: "Garçom"},
+        {id:2,nome:"Luiz", cargo: "Chapeiro"},
+        {id:3, nome: "Sebastian", cargo: "Segurança"}
+    ]
 
     return(
         <>
@@ -25,6 +34,19 @@ function App(){
                     key = {Lanche.id}
                     nome = {Lanche.nome}
                     preco = {Lanche.preco}
+                    imagem={Lanche.imagem}
+                    />
+                ))}
+                </div>
+
+                <div className="class_funcionario">
+
+                {
+                funcionario.map(funcionario => (
+                    <Funcionario
+                    key = {funcionario.id}
+                    nome = {funcionario.nome}
+                    cargo = {funcionario.cargo}
                     />
                 ))}
                 </div>
--- projeto-lanchonete/src/components/CardProd.jsx
+++ projeto-lanchonete/src/components/CardProd.jsx
@@ -1,17 +1,20 @@
 import './CardProd.css'
 
-function CardProd ({nome, preco}){
+function CardProd ({nome, preco, imagem}){
 
     return  (
 <>
+
+        <div className="tudo">
         <div className="produto">
+            <img src={imagem} alt="nome" />
             <h3>{nome}</h3>
         </div>
 
         <div className="preco">
             <p>{preco}</p>
         </div>
-
+        </div>
 </>
     )
 }
