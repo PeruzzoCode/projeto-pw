a7520364e3fcbc8810c0c77ec851a37ac11594dc
Pedro Peruzzo <peruzzo.code@gmail.com>
2026-08-05 17:14:17 -0300

    adicionando cor no botao mais e menos

--- projeto-lanchonete/src/components/Contador.jsx
+++ projeto-lanchonete/src/components/Contador.jsx
@@ -1,20 +1,20 @@
 import './Contador.css'
-import {useState} from "react";
+import './Contador.css' import {useState} from "react";
 
 function Contador(){
 
 const[contador,setContador] = useState(0)
 
-return(
-<>
+return( <>
 
-    <div className="Contador">
-    <h1>{contador}</h1>
-    <button onClick={() => setContador(contador + 1)}>Adicionar</button>
-    <button onClick={() => { if (contador > 0){setContador(contador - 1);}}}>Remover</button>
-    <button onClick={() => setContador(0)}>Zerar</button>
-    
-    </div>
+<div className="Contador">
+
+<button className = "menos" onClick={() => { if (contador > 0){setContador(contador - 1);}}}>-</button>
+<h1>{contador}</h1>
+<button className = "mais"onClick={() => setContador(contador + 1)}>+</button>
+
+
+</div>
     
 </>
 )
